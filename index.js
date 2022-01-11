b65bcb5f99e86f8728e8c98f95fa7e5131b54050
Amanda <amandamaiken@berkeley.edu>
2022-01-11 13:03:56 -0800

    commit tree 582b62f72bfbcd138515c8cec653f20bc4e5f030

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Code your solution in this file!
+const scuberHqBlock = 42;
+const blockLength = 264;
+
+function distanceFromHqInBlocks( blocks ) {
+    return Math.abs( blocks - scuberHqBlock );
+}
+
+function distanceFromHqInFeet( blocks ) {
+    return distanceFromHqInBlocks( blocks ) * blockLength;
+}
+
+function distanceTravelledInFeet( start, destination ) {
+    if ( start<destination ) {
+        return ( (destination-start) * blockLength );
+    } else {
+        return ( ( start-destination ) * blockLength );
+    }
+}
+
+function calculatesFarePrice( start, destination ) {
+    const distance = distanceTravelledInFeet( start, destination );
+    if ( distance <= 400 ) { 
+       return 0;
+    } else if ( distance >= 400 && distance <=2000 ) {
+       return 0.02 * ( distance - 400 )
+    } else if ( distance > 2000 && distance < 2500 ) {
+       return 25;
+    } else {
+        return 'cannot travel that far';
+    }
+  }
